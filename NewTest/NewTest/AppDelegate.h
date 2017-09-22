af27b206264e7a4f4fd4db646d72d8cf188cf510
zb <weifulin@zibmc.com>
2017-09-22 16:44:45 +0800

    MyTest

---
+++ NewTest/NewTest/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  NewTest
+//
+//  Created by zb on 2017/9/22.
+//  Copyright © 2017年 weifulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ NewTest/NewTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewTest
+//
+//  Created by zb on 2017/9/22.
+//  Copyright © 2017年 weifulin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
